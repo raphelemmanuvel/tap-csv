d5630ba320e462cc890cfc88cef48843eaa72665
raphelemmanuvel <>
2024-09-25 11:55:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,160 @@
+# `tap-csv`
+A Singer Tap for extracting data from CSV files built using the Meltano SDK.
+
+CSV tap class.
+
+Built with the [Meltano SDK](https://sdk.meltano.com) for Singer Taps and Targets.
+
+## Capabilities
+
+* `sync`
+* `catalog`
+* `discover`
+
+Note: This tap currently does not support incremental state.
+
+## Settings
+
+| Setting             | Required | Default | Description |
+|:--------------------|:--------:|:-------:|:------------|
+| files               | False    | None    | An array of csv file stream settings. |
+| csv_files_definition| False    | None    | A path to the JSON file holding an array of file settings. |
+| add_metadata_columns| False    | False   | When True, add the metadata columns (`_sdc_source_file`, `_sdc_source_file_mtime`, `_sdc_source_lineno`) to output. |
+| custom_mappings			| False		 | None    | An array of custom mapping settings for csv columns.	|
+
+A full list of supported settings and capabilities is available by running: `tap-csv --about`
+
+The `config.json` contains an array called `files` that consists of dictionary objects detailing each destination table to be passed to Singer. Each of those entries contains:
+* `entity`: The entity name to be passed to singer (i.e. the table)
+* `path`: Local path to the file to be ingested. Note that this may be a directory, in which case all files in that directory and any of its subdirectories will be recursively processed
+* `keys`: The names of the columns that constitute the unique keys for that entity
+* `encoding`: [Optional] The file encoding to use when reading the file (i.e. "latin1", "UTF-8"). Use this setting when you get a `UnicodeDecodeError` error.
+
+The following entries are passed through in an internal CSV dialect that then is used to configure the CSV reader:
+* `delimiter`: A one-character string used to separate fields. It defaults to ','.
+* `doublequote`: Controls how instances of quotechar appearing inside a field should themselves be quoted. When True, the character is doubled. When False, the escapechar is used as a prefix to the quotechar. It defaults to True.
+* `escapechar`: A one-character string used by the reader, where the escapechar removes any special meaning from the following character. It defaults to None, which disables escaping.
+* `quotechar`: A one-character string used to quote fields containing special characters, such as the delimiter or quotechar, or which contain new-line characters. It defaults to '"'.
+* `skipinitialspace`: When True, spaces immediately following the delimiter are ignored. The default is False.
+* `strict`: When True, raise exception Error on bad CSV input. The default is False.
+
+Example:
+
+```json
+{
+	"files":	[
+					{	"entity" : "leads",
+						"path" : "/path/to/leads.csv",
+						"keys" : ["Id"],
+						"delimiter": ";"
+					},
+					{	"entity" : "opportunities",
+						"path" : "/path/to/opportunities.csv",
+						"keys" : ["Id"],
+						"encoding" : "latin1",
+						"skipinitialspace": true
+					}
+				]
+}
+```
+
+Optionally, the files definition can be provided by an external json file:
+
+**config.json**
+```json
+{
+	"csv_files_definition": "files_def.json"
+}
+```
+
+
+**files_def.json**
+```json
+[
+	{	"entity" : "leads",
+		"path" : "/path/to/leads.csv",
+		"keys" : ["Id"]
+	},
+	{	"entity" : "opportunities",
+		"path" : "/path/to/opportunities.csv",
+		"keys" : ["Id"]
+	}
+]
+```
+
+## Installation
+
+```bash
+pipx install git+https://github.com/MeltanoLabs/tap-csv.git
+```
+
+## Usage
+
+You can easily run `tap-csv` by itself or in a pipeline using [Meltano](https://meltano.com/).
+
+### Executing the Tap Directly
+
+```bash
+tap-csv --version
+tap-csv --help
+tap-csv --config CONFIG --discover > ./catalog.json
+```
+
+## Developer Resources
+
+### Initialize your Development Environment
+
+```bash
+pipx install poetry
+poetry install
+```
+
+### Create and Run Tests
+
+Create tests within the `tap_csv/tests` subfolder and
+  then run:
+
+```bash
+poetry run tox
+poetry run tox -e pytest
+poetry run tox -e format
+poetry run tox -e lint
+```
+
+You can also test the `tap-csv` CLI interface directly using `poetry run`:
+
+```bash
+poetry run tap-csv --help
+```
+
+### Testing with [Meltano](https://www.meltano.com)
+
+_**Note:** This tap will work in any Singer environment and does not require Meltano.
+Examples here are for convenience and to streamline end-to-end orchestration scenarios._
+
+Your project comes with a custom `meltano.yml` project file already created. Open the `meltano.yml` and follow any _"TODO"_ items listed in
+the file.
+
+Next, install Meltano (if you haven't already) and any needed plugins:
+
+```bash
+# Install meltano
+pipx install meltano
+# Initialize meltano within this directory
+cd tap-csv
+meltano install
+```
+
+Now you can test and orchestrate using Meltano:
+
+```bash
+# Test invocation:
+meltano invoke tap-csv --version
+# OR run a test `elt` pipeline:
+meltano elt tap-csv target-jsonl
+```
+
+### SDK Dev Guide
+
+See the [dev guide](https://sdk.meltano.com/en/latest/dev_guide.html) for more instructions on how to use the SDK to
+develop your own taps and targets.
---
+++ tap_csv/client.py
@@ -0,0 +1,245 @@
+"""Custom client handling, including CSVStream base class."""
+
+from __future__ import annotations
+
+import csv
+import os
+import typing as t
+from datetime import datetime, timezone
+
+from singer_sdk import typing as th
+from singer_sdk.streams import Stream
+
+if t.TYPE_CHECKING:
+    from singer_sdk.helpers.types import Context
+
+SDC_SOURCE_FILE_COLUMN = "_sdc_source_file"
+SDC_SOURCE_LINENO_COLUMN = "_sdc_source_lineno"
+SDC_SOURCE_FILE_MTIME_COLUMN = "_sdc_source_file_mtime"
+
+
+class CSVStream(Stream):
+    """Stream class for CSV streams."""
+
+    file_paths: list[str] = []  # noqa: RUF012
+    header: list[str] = []  # noqa: RUF012
+
+    def __init__(self, *args, **kwargs):
+        """Init CSVStram."""
+        # cache file_config so we dont need to go iterating the config list again later
+        self.file_config = kwargs.pop("file_config")
+        super().__init__(*args, **kwargs)
+
+    def get_records(self, context: Context | None) -> t.Iterable[dict]:
+        """Return a generator of row-type dictionary objects.
+
+        The optional `context` argument is used to identify a specific slice of the
+        stream if partitioning is required for the stream. Most implementations do not
+        require partitioning and should ignore the `context` argument.
+        """
+        schema = self.schema  # Get schema from the catalog
+
+        date_columns = self._get_date_columns(schema)
+        int_columns = self._get_columns_by_type(schema, "integer")
+        double_columns = self._get_columns_by_type(schema, "double")
+        string_columns = self._get_columns_by_type(schema, "string")
+
+        for file_path in self.get_file_paths():
+            file_last_modified = datetime.fromtimestamp(
+                os.path.getmtime(file_path), timezone.utc
+            )
+
+            file_lineno = -1
+
+            for row in self.get_rows(file_path):
+                file_lineno += 1
+
+                if not file_lineno:
+                    continue
+
+                if self.config.get("add_metadata_columns", False):
+                    row = [file_path, file_last_modified, file_lineno, *row]
+
+                row_dict = dict(zip(self.header, row))
+                self._apply_transformations(row_dict, date_columns, int_columns, double_columns, string_columns)
+                yield row_dict
+
+    def _apply_transformations(self, row_dict: dict, date_columns: list[str], int_columns: list[str],
+                               double_columns: list[str], string_columns: list[str]) -> None:
+        """Apply necessary transformations to the row based on column type."""
+        for col in date_columns:
+            if col in row_dict:
+                row_dict[col] = self._transform_date(row_dict[col])
+
+        for col in int_columns:
+            if col in row_dict:
+                row_dict[col] = self._safe_cast(row_dict[col], int)
+
+        for col in double_columns:
+            if col in row_dict:
+                row_dict[col] = self._safe_cast(row_dict[col], float)
+
+        for col in string_columns:
+            if col in row_dict:
+                row_dict[col] = str(row_dict[col])
+
+    def _safe_cast(self, value: str, target_type: t.Type) -> t.Any:
+        """Safely cast a value to the target type."""
+        try:
+            return target_type(value)
+        except ValueError:
+            self.logger.warning(f"Failed to cast {value} to {target_type}")
+            return value
+
+    def _get_recursive_file_paths(self, file_path: str) -> list:
+        file_paths = []
+
+        for dirpath, _, filenames in os.walk(file_path):
+            for filename in filenames:
+                file_path = os.path.join(dirpath, filename)
+                if self.is_valid_filename(file_path):
+                    file_paths.append(file_path)
+
+        return file_paths
+
+
+    def _transform_date(self, date_str: str) -> str:
+        """Transform date from MM/DD/YY to YYYY-MM-DD."""
+        try:
+            # Parse and reformat the date
+            return  datetime.strptime(date_str, '%m/%d/%y').strftime('%Y-%m-%d')
+        except ValueError:
+            self.logger.warning(f"Date format not recognized for value: {date_str}")
+            return date_str  # Return the original value if parsing fails
+
+    def _get_date_columns(self, schema: dict) -> list[str]:
+        """Extract columns from the schema that are of type 'date'."""
+        date_columns = []
+        for field, field_schema in schema.get("properties", {}).items():
+            if "string" in field_schema.get("type", []) and field_schema.get("format") == "date":
+                date_columns.append(field)
+        return date_columns
+
+
+    def _get_columns_by_type(self, schema: dict, data_type: str) -> list[str]:
+        """Extract columns from the schema that match the specified data type."""
+        return [
+            field
+            for field, field_schema in schema.get("properties", {}).items()
+            if data_type in field_schema.get("type", [])
+        ]
+
+    def get_file_paths(self) -> list:
+        """Return a list of file paths to read.
+
+        This tap accepts file names and directories so it will detect
+        directories and iterate files inside.
+        """
+        # Cache file paths so we dont have to iterate multiple times
+        if self.file_paths:
+            return self.file_paths
+
+        file_path = self.file_config["path"]
+        if not os.path.exists(file_path):
+            raise Exception(f"File path does not exist {file_path}")
+
+        file_paths = []
+        if os.path.isdir(file_path):
+            clean_file_path = os.path.normpath(file_path) + os.sep
+            file_paths = self._get_recursive_file_paths(clean_file_path)
+        elif self.is_valid_filename(file_path):
+            file_paths.append(file_path)
+
+        if not file_paths:
+            raise RuntimeError(
+                f"Stream '{self.name}' has no acceptable files. \
+                    See warning for more detail."
+            )
+        self.file_paths = file_paths
+        return file_paths
+
+    def is_valid_filename(self, file_path: str) -> bool:
+        """Return a boolean of whether the file includes CSV extension."""
+        is_valid = True
+        if file_path[-4:] != ".csv":
+            is_valid = False
+            self.logger.warning(f"Skipping non-csv file '{file_path}'")
+            self.logger.warning(
+                "Please provide a CSV file that ends with '.csv'; e.g. 'users.csv'"
+            )
+        return is_valid
+
+    def get_rows(self, file_path: str) -> t.Iterable[list]:
+        """Return a generator of the rows in a particular CSV file."""
+        encoding = self.file_config.get("encoding", None)
+        csv.register_dialect(
+            "tap_dialect",
+            delimiter=self.file_config.get("delimiter", ","),
+            doublequote=self.file_config.get("doublequote", True),
+            escapechar=self.file_config.get("escapechar", None),
+            quotechar=self.file_config.get("quotechar", '"'),
+            skipinitialspace=self.file_config.get("skipinitialspace", False),
+            strict=self.file_config.get("strict", False),
+        )
+        with open(file_path, encoding=encoding) as f:
+            yield from csv.reader(f, dialect="tap_dialect")
+
+    @property
+    def schema(self) -> dict:
+        """Return dictionary of record schema.
+
+        Dynamically detect the json schema for the stream.
+        This is evaluated prior to any records being retrieved.
+        """
+        properties: list[th.Property] = []
+        self.primary_keys = self.file_config.get("keys", [])
+
+
+        custom_mappings = self.config.get("custom_mappings", [])
+
+        for file_path in self.get_file_paths():
+            for header in self.get_rows(file_path):  # noqa: B007
+                break
+            break
+
+        # Check each column header and assign date format if it's in the config list
+        for column in header:
+
+            mapping = next((item for item in custom_mappings if item['key'] == column), None)
+            if mapping is not None:
+
+                if mapping["data_type"] == "date":
+                    properties.append(th.Property(column, th.DateType()))
+
+                elif mapping["data_type"] == "integer":
+                    properties.append(th.Property(column, th.IntegerType()))
+
+                elif mapping["data_type"] == "double":
+                    properties.append(th.Property(column, th.DoubleType()))
+
+                elif mapping["data_type"] == "string":
+                    properties.append(th.Property(column, th.StringType()))
+            else:
+                # Default to string type if no mapping is found for the column
+                properties.append(th.Property(column, th.StringType()))
+
+        # If enabled, add file's metadata to output
+        if self.config.get("add_metadata_columns", False):
+            header = [
+                SDC_SOURCE_FILE_COLUMN,
+                SDC_SOURCE_FILE_MTIME_COLUMN,
+                SDC_SOURCE_LINENO_COLUMN,
+                *header,
+            ]
+
+            properties.extend(
+                (
+                    th.Property(SDC_SOURCE_FILE_COLUMN, th.StringType),
+                    th.Property(SDC_SOURCE_FILE_MTIME_COLUMN, th.DateTimeType),
+                    th.Property(SDC_SOURCE_LINENO_COLUMN, th.IntegerType),
+                )
+            )
+        # Cache header for future use
+        self.header = header
+
+        return th.PropertiesList(*properties).to_dict()
